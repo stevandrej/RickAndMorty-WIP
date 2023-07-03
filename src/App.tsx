dc2c9a2467dd6600187bf90b0115fbb50879fcb3
Andrej Stevkovski <stevandrej@yahoo.com>
2023-07-04 01:51:15 +0200

    client [fix]: url construction keep /api appendix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -9,45 +9,49 @@ import Locations from "./components/Locations";
 import Episodes from "./components/Episodes";
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+		},
+	},
 });
 
 function App() {
-  const [count, setCount] = useState(0);
+	const [count, setCount] = useState(0);
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Characters />
-      <Locations />
-      <Episodes />
+	return (
+		<QueryClientProvider client={queryClient}>
+			<div>
+				<a href="https://vitejs.dev" target="_blank">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank">
+					<img
+						src={reactLogo}
+						className="logo react"
+						alt="React logo"
+					/>
+				</a>
+			</div>
+			<h1>Vite + React</h1>
+			<div className="card">
+				<button onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className="read-the-docs">
+				Click on the Vite and React logos to learn more
+			</p>
+			<Characters />
+			<Locations />
+			<Episodes />
 
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
 }
 
 export default App;
--- src/services/client.tsx
+++ src/services/client.tsx
@@ -1,36 +1,36 @@
 interface RequestOptions extends RequestInit {
-  params?: Record<number | string, number | string>;
+	params?: Record<number | string, number | string>;
 }
 
 export function client<T>(
-  url: string,
-  options: RequestOptions = {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }
+	url: string,
+	options: RequestOptions = {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+		},
+	}
 ): Promise<T> {
-  const baseUrl = "https://rickandmortyapi.com/api";
-  const requestUrl = new URL(url, baseUrl);
+	const baseUrl = "https://rickandmortyapi.com/api";
+	const requestUrl = new URL(baseUrl + url);
 
-  // Add URL parameters
-  if (options.params) {
-    Object.entries(options.params).forEach(([key, value]) => {
-      requestUrl.searchParams.append(key, value.toString());
-    });
-    delete options.params; // Remove the params property from options
-  }
+	// Add URL parameters
+	if (options.params) {
+		Object.entries(options.params).forEach(([key, value]) => {
+			requestUrl.searchParams.append(key, value.toString());
+		});
+		delete options.params; // Remove the params property from options
+	}
 
-  return fetch(requestUrl.href, options)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Request failed");
-      }
-      return response.json();
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      return Promise.reject(error);
-    });
+	return fetch(requestUrl.href, options)
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("Request failed");
+			}
+			return response.json();
+		})
+		.catch((error) => {
+			console.error("Error:", error);
+			return Promise.reject(error);
+		});
 }
