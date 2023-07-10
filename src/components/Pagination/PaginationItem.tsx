20ea3dedbf6380cef8ef33b85f660bf9af248ef1
Andrej Stevkovski <stevandrej@yahoo.com>
2023-07-11 00:33:14 +0200

    pagination: UI, colors

--- src/components/Pagination/PaginationItem.tsx
+++ src/components/Pagination/PaginationItem.tsx
@@ -8,12 +8,16 @@ interface PaginationItemProps {
 
 const PaginationItem = React.memo(
   ({ page, handleClick, isActive }: PaginationItemProps) => {
-    const activeClassName = isActive ? "bg-sky-600 text-white" : "";
+    const activeClassName = isActive
+      ? "bg-sky-700 text-white"
+      : "text-sky-700 ";
 
     return (
       <li>
         <button
-          className={`border border-slate-600 rounded-full flex items-center justify-center p-4 w-8 h-8 ${activeClassName}`}
+          className={`shadow-md shadow-sky-700/50
+            ring-1 ring-opacity-20 ring-sky-700 
+            rounded-full flex items-center justify-center p-4 w-8 h-8 ${activeClassName}`}
           disabled={isActive}
           onClick={() => handleClick(page)}
         >
