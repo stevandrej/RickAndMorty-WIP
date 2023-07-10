0025ed3a1a85339dded18b8857dac82fc47fb17f
Andrej Stevkovski <stevandrej@yahoo.com>
2023-07-10 13:04:08 +0200

    pagination optimisation

--- src/components/Pagination/PaginationItem.tsx
+++ src/components/Pagination/PaginationItem.tsx
@@ -1,22 +1,24 @@
+import React from "react";
+
 interface PaginationItemProps {
   page: number;
   handleClick: (page: number) => void;
   isActive: boolean;
 }
 
-export default function PaginationItem({
-  page,
-  handleClick,
-  isActive,
-}: PaginationItemProps) {
-  return (
-    <li
-      className={`border border-slate-600 rounded-full p-4 w-8 h-8 flex items-center justify-center cursor-pointer ${
-        isActive && "bg-sky-600 text-white"
-      }`}
-      onClick={() => handleClick(page)}
-    >
-      {page}
-    </li>
-  );
-}
+const PaginationItem = React.memo(
+  ({ page, handleClick, isActive }: PaginationItemProps) => {
+    return (
+      <li
+        className={`border border-slate-600 rounded-full p-4 w-8 h-8 flex items-center justify-center cursor-pointer ${
+          isActive && "bg-sky-600 text-white"
+        }`}
+        onClick={() => handleClick(page)}
+      >
+        {page}
+      </li>
+    );
+  }
+);
+
+export default PaginationItem;
