e64145b25ffe424a4a28509b6841b8d95234cb59
Andrej Stevkovski <stevandrej@yahoo.com>
2023-07-11 12:32:22 +0200

    fix: url dash

--- src/queries/useGetCharacter.ts
+++ src/queries/useGetCharacter.ts
@@ -2,10 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import { client } from "../services/client";
 import { Character } from "../types/Character";
 
-export default function useGetCharacter(id: number) {
+export default function useGetCharacter(id = "") {
   const results = useQuery<Character>({
     queryKey: ["character", id],
-    queryFn: () => client(`character/${id}`),
+    queryFn: () => client(`/character/${id}`),
   });
 
   return { ...results };
--- src/queries/useGetLocation.ts
+++ src/queries/useGetLocation.ts
@@ -4,7 +4,7 @@ import { client } from "../services/client";
 export default function useGetLocation(id: number) {
   const results = useQuery<Location>({
     queryKey: ["location", id],
-    queryFn: () => client(`location/${id}`),
+    queryFn: () => client(`/location/${id}`),
   });
 
   return { ...results };
